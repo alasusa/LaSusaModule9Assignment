bda70d6e197cdedc5931ffa345a7253127543be4
Aidan LaSusa <114534091+alasusa@users.noreply.github.com>
2024-04-04 22:12:50 -0400

    Update Driver.java

--- src/main/java/org/example/Driver.java
+++ src/main/java/org/example/Driver.java
@@ -3,13 +3,18 @@ package org.example;
 import java.util.Scanner;
 
 public class Driver {
+    // METHOD TO CREATE AN OUTFIT THROUGH APPLICATION
     private static Application configureApplication() {
-
+System.out.println("Which Outfit would you like?");
+        //initiailzes a new outfit application and an outfitfactory
         Application app;
         OutfitFactory factory;
+
+        //asks for user input on what outfit they want
         Scanner scn = new Scanner(System.in);
         String printName = scn.nextLine();
 
+        //Checks to see if they want a specific type of outfit, will repeat until they input a correct outfit type
         if (printName.contains("Casual")) {
             factory = new CasualFactory();
             app = new Application(factory);
@@ -24,13 +29,13 @@ public class Driver {
             return app;
         } else {
             System.out.println("Please input whetehr you want a Casual, Party, or Professional outift.");
-            return null;
+            return configureApplication;
 
         }
     }
 
+    //main method creates an outift application from user input then prints the outfit details
     public static void main(String[] args) {
-        System.out.println("Which Outfit would you like?");
         Application app = configureApplication();
         app.revealContent();
     }
