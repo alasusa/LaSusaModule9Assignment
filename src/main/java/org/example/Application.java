53dab5a0610e3c553cd090a8ae03b4bd7fb56b5e
Aidan LaSusa <114534091+alasusa@users.noreply.github.com>
2024-04-04 22:09:01 -0400

    Update Application.java

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -3,16 +3,19 @@ package org.example;
 //CLASS TO ALLOW USER TO SPECIFY WHAT CLOTHES THEY WANT
 public class Application {
 
+    //Initializing the outfit
     private Pants pairOfPants ;
     private Tops Top;
     private Shoes pairOfShoes;
 
+    // creates an outfit
     public Application(OutfitFactory factory) {
        pairOfPants = factory.createAPants();
        Top = factory.createATop();
        pairOfShoes = factory.createAShoes();
     }
 
+    //shows outfit details
     public void revealContent(){
         pairOfPants.typeOfPants();
         Top.typeOfShirts();
